8b9d08bdf5545b9d6954cdba0c3917c2f9666163
Jay Quach <jay@backbase.com>
2016-05-31 14:53:14 -0400

    added cadidate surveys

--- client/app/components/comparisons/comparisons.controller.js
+++ client/app/components/comparisons/comparisons.controller.js
@@ -11,6 +11,7 @@ class ComparisonsController {
 		this.criteria = this.generateCriteria();
 		this.opinionOptions = this.generateOpinionOptions();
 		this.criteriaSurveys = this.composeSurveys(this.criteria);
+		this.candidateSurveys = this.composeCandidateSurveys(this.candidates);
 		this.calculateCriteriaWeights();
 	}
 
@@ -59,20 +60,41 @@ class ComparisonsController {
 		return surveys;
 	}
 
+	composeCandidateSurveys(options) {
+		let surveys = [];
+		let criteria = this.criteria;
+
+		criteria.forEach(c => {
+			for(let i = 0; i < options.length - 1; i++) {
+				for(let j = i +1; j < options.length; j++) {
+					surveys.push({
+						left: options[i], 
+						right: options[j],
+						criterion: c,
+						opinion: this.opinionOptions[4]});
+				}
+			}
+		});
+		
+
+		return surveys;
+	}
+
 	generateCandidates() {
 		return [
 			new Candidate('Candidate A', 'A is an awesome candidate'),
-			new Candidate('Candidate B', 'B is an ugly candidate')
+			new Candidate('Candidate B', 'B is an ugly candidate'),
+			new Candidate('Candidate C', 'C is an dumb candidate'),
 		];
 	}
 
 	generateCriteria() {
 		return [
-			new Criterion('Budget', 0),
-			new Criterion('Aesthetics', 0),
-			new Criterion('Maintenance Cost', 0),
-			new Criterion('Resell Value', 0),
-			new Criterion('Customizability', 0)
+			new Criterion('Budget', 1),
+			new Criterion('Aesthetics', 1),
+			new Criterion('Maintenance Cost', 1),
+			new Criterion('Resell Value', 1),
+			new Criterion('Customizability', 1)
 		];
 	}
 }
